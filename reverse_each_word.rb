590591bd9072e1ec9e254ef23f8a07df5cdbd83c
Matthew C <m23.cullen@gmail.com>
2019-08-05 16:24:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
 def reverse_each_word(string)
-  string = string.split(" ")
-  reverse = []
-  new_string = ""
-  string.each { |n| reverse = n.split("").unshift}
-  return reverse.join("")
+  
 end
